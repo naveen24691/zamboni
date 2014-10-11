b009ab69624dc0b19aff0892b9833d9dc6e068fa
Naveen Sinha <naveen24691@gmail.com>
2014-10-12 04:19:27 +0530

    Removing deprecated urls

--- mkt/site/urls.py
+++ mkt/site/urls.py
@@ -23,14 +23,9 @@ urlpatterns = patterns('',
     url('^opensearch.xml$', views.OpensearchView.as_view(), name='opensearch'),
 
     # These are the new manifest URLs going forward.
-    url('^hosted.webapp$', views.manifest, name='hosted.webapp'),
     url('^packaged.webapp$', views.package_minifest, name='packaged.webapp'),
     url('^marketplace-package.webapp$', views.yogafire_minifest,
         name='packaged-marketplace.webapp'),
-
-    # TODO: Deprecate this in favour of the ones above.
-    url('^manifest.webapp$', views.manifest, name='manifest.webapp'),
-    url('^minifest.webapp$', views.package_minifest, name='minifest.webapp'),
 
     url(r'^wafflejs$', wafflejs, name='wafflejs'),
     ('^services/', include(services_patterns)),
